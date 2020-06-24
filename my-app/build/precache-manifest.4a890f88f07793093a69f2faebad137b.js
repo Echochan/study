ad4075ef0b836bad23f3f84ee2d744729dfed401
Echochan <2605592447@qq.com>
2020-06-24 14:19:27 +0800

    study 202006241419

---
+++ my-app/build/precache-manifest.4a890f88f07793093a69f2faebad137b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c4b9721c33f89fef419796093e1655",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffcebff1e5c04c4423c9",
+    "url": "/static/css/main.01b7bd77.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcebff1e5c04c4423c9",
+    "url": "/static/js/main.d2e36f10.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
---
+++ my-app/src/game.js
@@ -0,0 +1,194 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+/* class Square extends React.Component {
+  //  constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     value: null
+  //   }
+  // } 
+  render() {
+    return (
+      <button 
+        className="square" 
+        onClick={() => { this.props.onClick() }}
+      >
+        {this.props.value}
+      </button>
+    )
+  }
+} */
+function Square(props) {
+  return (
+  <button className="square" onClick={props.onClick}>
+    {props.value}
+    </button>
+  )
+
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+class Board extends React.Component {
+  /* constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  } */
+  // handleClick(i) {
+  //   // 不可变性以及不可变性的重要性
+  //   const  squares = this.state.squares.slice()
+  //   if(calculateWinner(squares) || squares[i]) {
+  //     return
+  //   }
+  //   squares[i] = this.state.xIsNext ? 'X' : 'O'
+  //   this.setState({
+  //     squares: squares,
+  //     xIsNext: !this.state.xIsNext
+  //   })
+  // }
+  renderSquare(i) {
+    return <Square 
+    value={this.props.squares[i]}
+    onClick={ () => {this.props.onClick(i)}}
+    />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true,
+      stempNumber:0,
+    }
+  }
+  handleClick(i) {
+    // 不可变性以及不可变性的重要性
+    const history = this.state.history.slice(0, this.state.stempNumber + 1)
+    const current = history[history.length -1]
+    const squares = current.squares.slice()
+    if(calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = this.state.xIsNext ? '黑' : '白'
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext,
+      stempNumber: history.length
+    })
+  }
+
+  jumpTo(step) {
+    console.log(step)
+    if(step == 0) {
+      this.setState({
+        history: [{
+          squares: Array(9).fill(null)
+        }],
+        xIsNext: true,
+        stempNumber:0,
+      })
+      return
+    }
+    this.setState({
+      stempNumber: step,
+      xIsNext: step % 2 === 0
+    })
+  }
+
+  render() {
+    const history = this.state.history
+    const current = history[this.state.stempNumber]
+    const winner = calculateWinner(current.squares)
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 
+      `跳回第${move}步`:
+      `初始化`
+
+      return (
+        <li key={move}>
+          <button onClick={() => {this.jumpTo(move)}}>{desc}</button>
+        </li>
+      )
+    })
+
+    let status
+    if(winner) {
+      status = `获胜者: ${winner}`
+    }else {
+      status = `下一位：${this.state.xIsNext ? '黑' : '白'}`
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board 
+            squares = {current.squares}
+            onClick = {i => {this.handleClick(i)}}
+          />
+        </div>
+        <div className="game-info">
+          <div>{ status }</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  // <h1>Hello,world</h1>,
+  <Game />,
+  document.getElementById('root')
+)
